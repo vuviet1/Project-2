de92b4060d7a2a5beed77f6c22f3e2114c5df8e8
vuviet1 <92284575+vuviet1@users.noreply.github.com>
2023-10-02 14:26:56 +0700

    moi

--- resources/views/Management/SchoolYear/editSchoolyear.blade.php
+++ resources/views/Management/SchoolYear/editSchoolyear.blade.php
@@ -1,9 +1,9 @@
-<div class="modal fade" id="staticBackdropAdd" data-backdrop="static" data-keyboard="false"
+<div class="modal fade" id="staticBackdropEdit" data-backdrop="static" data-keyboard="false"
      tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Form thêm niên khóa</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Form sửa niên khóa</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
--- resources/views/Management/SchoolYear/schoolyear.blade.php
+++ resources/views/Management/SchoolYear/schoolyear.blade.php
@@ -5,53 +5,19 @@
 @endsection
 
 @section('content')
+    @include('Management.SchoolYear.addSchoolyear')
+    @include('Management.SchoolYear.editSchoolyear')
     <div class="container-fluid">
         <div class="container-fluid">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title fw-semibold mb-4">Niên khóa</h5>
 
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+                    <!-- Button trigger modal Add-->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdropAdd">
                         Thêm mới
                     </button>
-
                     <!-- Modal -->
-                    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false"
-                         tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="staticBackdropLabel">Form thêm niên khóa</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="card-body">
-                                        <div class="row g-3">
-                                            <div class="col">
-                                                <label for="idClass" class="form-label">Mã niên khóa</label>
-                                                <input type="text" class="form-control"
-                                                       placeholder="Mã niên khóa"
-                                                       name="id" disabled>
-                                            </div>
-                                            <div class="col">
-                                                <label for="nameStudent" class="form-label">Niên khóa</label>
-                                                <input type="date" class="form-control"
-                                                       placeholder="Niên khóa"
-                                                       name="number_course">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary">Thêm mới</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
 
                     {{-- End modal--}}
 
@@ -71,9 +37,9 @@
                                         <td>{{$f->id}}</td>
                                         <td>{{$f->number_course}}</td>
                                         <td>
-                                            <!-- Button to trigger the modal -->
-                                            <button class="btn btn-primary" type="button" data-bs-toggle="modal"
-                                                    data-bs-target="#staticBackdrop">Open Modal
+                                            <!-- Button to trigger the modal Edit-->
+                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdropEdit">
+                                                Sửa
                                             </button>
                                         </td>
                                     </tr>
