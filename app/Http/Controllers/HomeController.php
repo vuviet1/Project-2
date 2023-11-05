ae8ff1cc5ab8e01f2f5bfe61a3c075aacda24d7e
vuviet1 <92284575+vuviet1@users.noreply.github.com>
2023-11-05 23:25:55 +0700

    moi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = Student::select('id', 'created_at')->get()->groupBy(function ($data){
+           return Carbon::parse($data->created_at)->format('Y');
+        });
+        $years = [];
+        $yearsCount = [];
+        foreach ($data as $year => $value){
+            $years[] = $year;
+            $yearsCount[] = count($value);
+        }
+        return view('home', ['years'=>$years, 'yearsCount' => $yearsCount]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,45 @@
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         </div>
 
-         <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">
-                                {{ __('You are logged in!') }}
-                            </p>
-                        </div>
+        <div class="row">
+            <div class="col-xl-3 col-md-6">
+                <div class="card bg-primary text-white mb-4">
+                    <div class="card-body">Departments</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <a class="small text-white stretched-link" href="{{url('depart')}}">View Details</a>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
-            <!-- /.row -->
+            <div class="col-xl-3 col-md-6">
+                <div class="card bg-warning text-white mb-4">
+                    <div class="card-body">Employees</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <a class="small text-white stretched-link" href="{{url('employee')}}">View Details</a>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-chart-bar me-1"></i>
+                        Bar Chart Example
+                    </div>
+                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
     <!-- /.container-fluid -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        var _ydata=JSON.parse('{!! json_encode($years) !!}');
+        var _xdata=JSON.parse('{!! json_encode($yearsCount) !!}');
+    </script>
+    <script src="{{asset('public')}}/build/assets/demo/chart-bar-demo.js"></script>
 @endsection
