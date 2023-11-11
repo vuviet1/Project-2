d5ac1ab8c90a30f14439cc4e65e89748463fc299
vuviet1 <92284575+vuviet1@users.noreply.github.com>
2023-11-11 11:13:45 +0700

    moi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,12 @@
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
+{{--                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>--}}
+                            <img src="images/bkacad.png" alt="" class="row col-lg-7 d-none d-lg-block">
+                        <div class="col-lg-5">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">{{ __('Welcome Back!') }}</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">{{ __('Chào mừng trở lại !') }}</h1>
                                 </div>
                                 <form action="{{ route('login') }}" method="post" class="user">
                                     @csrf
@@ -23,7 +24,7 @@
                                         <input type="email" name="email" value="{{ old('email') }}"
                                                class="form-control form-control-user @error('email') is-invalid @enderror"
                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                               placeholder="{{ __('Enter Email Address') }}" required autofocus>
+                                               placeholder="{{ __('Nhập Email') }}" required autofocus>
                                     </div>
                                     @error('email')
                                     <div class="form-group custom-control">
@@ -33,20 +34,13 @@
 
                                     <div class="form-group">
                                         <input type="password" name="password" class="form-control form-control-user @error('password') is-invalid @enderror"
-                                               id="exampleInputPassword" placeholder="{{ __('Password') }}" required>
+                                               id="exampleInputPassword" placeholder="{{ __('Mật khẩu') }}" required>
                                     </div>
                                     @error('password')
                                     <div class="form-group custom-control">
                                         <label class="">{{ $message }}</label>
                                     </div>
                                     @enderror
-
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" name="remember" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">{{ __('Remember Me') }}</label>
-                                        </div>
-                                    </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
                                         {{ __('Login') }}
                                     </button>
